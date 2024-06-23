b0f4f43e4da24b971968f1d87bf8e18d38badb88
fernando <fernandot371@gmail.com>
2024-06-23 14:47:16 -0600

    routes, fles and folders components and tailwind

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aapp-user-loader',
+  standalone: true,
+  imports: [],
+  template: `<h1>Hola mundo</h1>`
+})
+
+export class UsersLoaderComponent  {
+  constructor() { }
+}
