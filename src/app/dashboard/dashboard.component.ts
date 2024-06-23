723f70ef9124771a78bd6a48d9e037466e60b9f2
fernando <fernandot371@gmail.com>
2024-06-23 15:12:52 -0600

    adding routes on the dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+// import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+import { SidemenuComponent } from '@shared/sidemenu/sidemenu.component'; // using a builded path on tsconfig.json
+
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SidemenuComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
