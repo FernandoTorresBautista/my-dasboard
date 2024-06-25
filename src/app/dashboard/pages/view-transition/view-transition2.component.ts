532bac30687e8e1af8fef37e65bd45c1833de8be
fernando <fernandot371@gmail.com>
2024-06-25 10:41:35 -0600

    example of animation

--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -9,17 +9,19 @@ import { TitleComponent } from '@shared/title/title.component';
 
     <app-title title="View Transition 2"></app-title>
 
-    <section class="flex justify-start">
+    <section class="flex justify-end">
 
       <img
         srcset="http://picsum.photos/id/237/200/300"
         alt="picsum"
         width="200"
         height="300"
+        style="view-transition-name: hero1"
       />
 
       <div
-        class="bg-blue-500 w-56 h-56"
+        class="fixed bottom-16 right-10 bg-blue-800 w-32 h-32 rounded"
+        style="view-transition-name: hero2"
       ></div>
 
     </section>
