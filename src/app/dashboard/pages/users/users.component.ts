94d3cb677e241638bc3c15bd3f9d63da7d178288
fernando <fernandot371@gmail.com>
2024-06-25 11:06:28 -0600

    peticiones http importProvidersFrom

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,17 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
 
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent, RouterModule],
   templateUrl: './users.component.html',
   styles: ``
 })
 export default class UsersComponent {
 
   public usersService = inject( UsersService );
+
+
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, signal } from '@angular/core';
-import { User } from '@interfaces/req-response';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { User, UsersResponse } from '@interfaces/req-response';
+import { delay } from 'rxjs';
 
 interface State {
   users: User[];
@@ -11,15 +13,29 @@ interface State {
 })
 export class UsersService {
 
+  private http = inject( HttpClient );
+
   // # -> para hacer el state provado, sin usar private
   #state = signal<State>({
     loading : true,
     users: [],
   })
 
+  public users = computed( () => this.#state().users );
+  public loading = computed( () => this.#state().loading );
+
   constructor() {
 
-    console.log('Cargando data')
+    console.log('Cargando data...')
+    this.http.get<UsersResponse>('https://reqres.in/api/users')
+      .pipe( delay(1500) )
+      .subscribe( res => {
+        this.#state.set({
+          loading: false,
+          users: res.data
+        })
+      });
+    console.log("cargado")
 
   }
 
